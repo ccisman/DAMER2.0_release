2bfe33ce8671c032c6a51a64487351addb8c3711
cc <ccisman@163.com>
2021-10-09 15:53:36 +0800

    增加部分实验，工具bug修复

---
+++ test_programs/t-30.c
@@ -0,0 +1,45 @@
+extern void abort(void);
+void reach_error(){}
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+
+#include <pthread.h>
+#include <assert.h>
+#define N 30
+
+// verify t-30.c
+
+int shared = 0;
+int judge = 0;
+
+void error(void)
+{
+  ERROR: {reach_error();abort();}
+  return;
+}
+
+void *
+thread(void *arg)
+{
+    shared = shared + 1;
+}
+
+void *
+thread1(void *arg)
+{
+    if(shared > 3) error();
+}
+
+int main(void)
+{
+	pthread_t t[N],t1;
+    for(int i=0;i<N;i++)
+	    pthread_create(&t[i], 0, thread, 0);
+    pthread_create(&t1, 0, thread1, 0);
+
+    for(int i=0;i<N;i++)
+	    pthread_join(t[i], 0);
+    pthread_join(t1, 0);
+    return 0;
+}
